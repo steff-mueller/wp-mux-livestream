f1f7d7b8b789c3db9f1a2c2b10aebfb635674ab8
Steffen Müller <arxisos@gmail.com>
2025-01-04 18:01:47 +0100

    Initial commit

---
+++ src/wp-mux-livestream/render.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
+ */
+?>
+<p <?php echo get_block_wrapper_attributes(); ?>>
+	<?php esc_html_e( 'Wp Mux Livestream – hello from a dynamic block!', 'wp-mux-livestream' ); ?>
+</p>
